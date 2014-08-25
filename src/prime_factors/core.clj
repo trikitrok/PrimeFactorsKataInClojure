fc3755253ff9143cfd1f6633904c76fa7ba65da9
trikitrok <trikitrok@gmail.com>
2014-08-25 10:55:57 +0200

    Refactor: Extracted a helper and used inc function

--- src/prime_factors/core.clj
+++ src/prime_factors/core.clj
@@ -1,8 +1,13 @@
 (ns prime-factors.core)
 
+(defn- multiple? [number prime]
+  (zero? (mod number prime)))
+
 (defn factorize [number]
   (loop [number number prime 2 factors []]
     (cond 
       (= number 1) factors
-      (zero? (mod number prime)) (recur (/ number prime) prime (conj factors prime))
-      :else (recur number (+ prime 1) factors))))
+      (multiple? number prime) (recur (/ number prime) prime (conj factors prime))
+      :else (recur number (inc prime) factors))))
+
+
