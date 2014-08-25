9668bfcc9d4bb49739f68e1624f3bafd025c5fa4
trikitrok <trikitrok@gmail.com>
2014-08-25 10:58:10 +0200

    Deleted midje sample message that I had forgotten to remove

--- test/prime_factors/core_test.clj
+++ test/prime_factors/core_test.clj
@@ -2,8 +2,6 @@
   (:use midje.sweet)
   (:use [prime-factors.core]))
 
-(println "You should expect to see three failures below.")
-
 (facts 
   "about prime factors computation"
   (fact "1 has no factors"
