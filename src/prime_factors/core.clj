a2c2802c88744561999096b4f7eefe9169cbf49c
trikitrok <trikitrok@gmail.com>
2014-08-25 11:09:25 +0200

    Renamed helper

--- src/prime_factors/core.clj
+++ src/prime_factors/core.clj
@@ -1,13 +1,11 @@
 (ns prime-factors.core)
 
-(defn- multiple? [number prime]
-  (zero? (mod number prime)))
+(def ^:private factor?
+  (comp zero? rem))
 
 (defn factorize [number]
   (loop [number number prime 2 factors []]
     (cond 
       (= number 1) factors
-      (multiple? number prime) (recur (/ number prime) prime (conj factors prime))
+      (factor? number prime) (recur (/ number prime) prime (conj factors prime))
       :else (recur number (inc prime) factors))))
-
-
